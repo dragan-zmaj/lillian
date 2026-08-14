f12e1c9563f5f0fc5133524c35cc027e022d555c
dragan-zmaj <lukabmx69@gmail.com>
2026-08-14 13:28:43 +0200

    change o1heap version

--- libs/o1heap/o1heap.h
+++ libs/o1heap/o1heap.h
@@ -11,7 +11,7 @@
 // OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-// Copyright (c) Pavel Kirienko
+// Copyright (c) 2020 Pavel Kirienko
 // Authors: Pavel Kirienko <pavel.kirienko@zubax.com>
 //
 // READ THE DOCUMENTATION IN README.md.
@@ -28,10 +28,10 @@ extern "C" {
 #endif
 
 /// The semantic version number of this distribution.
-#define O1HEAP_VERSION_MAJOR 3
+#define O1HEAP_VERSION_MAJOR 2
 
 /// The guaranteed alignment depends on the platform pointer width.
-#define O1HEAP_ALIGNMENT (sizeof(void*) * 2U)
+#define O1HEAP_ALIGNMENT (sizeof(void*) * 4U)
 
 /// The definition is private, so the user code can only operate on pointers. This is done to enforce encapsulation.
 typedef struct O1HeapInstance O1HeapInstance;
@@ -41,11 +41,6 @@ typedef struct O1HeapInstance O1HeapInstance;
 /// If assertion checks are not disabled, the library will perform automatic runtime self-diagnostics that trigger
 /// an assertion failure if a heap corruption is detected.
 /// Health checks and validation can be done with o1heapDoInvariantsHold().
-///
-/// TODO NOTICE: Maintenance of this information takes about a dozen cycles at least, which is quite significant
-/// compared to the amount of computation needed to do the actual memory management. In the future, we may add a
-/// preprocessor option that disables diagnostics for the benefit of the most performance-sensitive applications.
-/// If you find this feature relevant for your use case, consider opening a ticket.
 typedef struct
 {
     /// The total amount of memory available for serving allocation requests (heap size).
@@ -71,11 +66,6 @@ typedef struct
     uint64_t oom_count;
 } O1HeapDiagnostics;
 
-/// o1heapInit() will fail unless the arena size is at least this large.
-/// This value depends only on the machine architecture.
-/// The other reason to fail is if the arena pointer is not aligned at O1HEAP_ALIGNMENT.
-extern const size_t o1heapMinArenaSize;
-
 /// The arena base pointer shall be aligned at O1HEAP_ALIGNMENT, otherwise NULL is returned.
 ///
 /// The total heap capacity cannot exceed approx. (SIZE_MAX/2). If the arena size allows for a larger heap,
@@ -86,13 +76,10 @@ extern const size_t o1heapMinArenaSize;
 /// own needs (normally about 40..600 bytes depending on the architecture, but this parameter is not characterized).
 /// A pointer to the newly initialized instance is returned.
 ///
-/// The function fails and returns NULL iff:
-/// - The provided space is less than o1heapMinArenaSize.
-/// - The base pointer is not aligned at O1HEAP_ALIGNMENT.
-/// - The base pointer is NULL.
+/// If the provided space is insufficient, NULL is returned.
 ///
-/// An initialized instance does not hold any resources except for the arena memory.
-/// Therefore, if the instance is no longer needed, it can be discarded without any de-initialization procedures.
+/// An initialized instance does not hold any resources. Therefore, if the instance is no longer needed,
+/// it can be discarded without any de-initialization procedures.
 ///
 /// The heap is not thread-safe; external synchronization may be required.
 O1HeapInstance* o1heapInit(void* const base, const size_t size);
@@ -116,39 +103,6 @@ void* o1heapAllocate(O1HeapInstance* const handle, const size_t amount);
 ///
 /// The function is executed in constant time.
 void o1heapFree(O1HeapInstance* const handle, void* const pointer);
-
-/// Similar to the standard realloc() with a few improvements. Given a previously allocated fragment and a new size,
-/// attempts to resize the fragment.
-///
-/// - If the pointer is NULL, acts as o1heapAllocate(). The complexity is constant.
-///
-/// - If the new_amount is zero, acts as o1heapFree() (n.b.: in realloc() this case is implementation-defined).
-///   The result should be ignored. The complexity is constant.
-///
-/// - If the new_amount is not greater than the old fragment size, the same memory pointer is always returned;
-///   the data is not moved and the fragment is shrunk in place. The complexity is constant.
-///
-/// - If the new_amount is greater than the old fragment but there is enough free space after the fragment
-///   to expand in-place, the fragment is expanded in place and the same pointer is returned; the data is not moved.
-///   The complexity is constant. The contents of the expanded memory is undefined.
-///
-/// - If the new_amount is greater than the old fragment and there is not enough free space after the fragment to
-///   expand in-place, but there is a suitable free space elsewhere, the data is moved and the new pointer is returned.
-///   The complexity is LINEAR (sic!) of the size of the old fragment, as it needs to be memmove()d to the new location.
-///   The library attempts to relocate the data in a nearby or recently used area to improve cache locality.
-///   The new pointer is returned. The new fragment may overlap the original one.
-///
-/// - Otherwise, there is not enough memory to expand the fragment as requested. The old fragment remains valid as-is,
-///   and NULL is returned to indicate failure. The complexity is constant.
-///
-/// To summarize, the only linear-complexity case is when the new_amount is larger and there is not enough free space
-/// following this fragment, necessitating moving the data to a new place; the library avoids this if at all possible.
-/// Every other case is constant-complexity as the data is not moved.
-void* o1heapReallocate(O1HeapInstance* const handle, void* const pointer, const size_t new_amount);
-
-/// Obtains the maximum theoretically possible allocation size for this heap instance.
-/// This is useful when implementing std::allocator_traits<Alloc>::max_size.
-size_t o1heapGetMaxAllocationSize(const O1HeapInstance* const handle);
 
 /// Performs a basic sanity check on the heap.
 /// This function can be used as a weak but fast method of heap corruption detection.
--- libs/o1heap/readme.md
+++ libs/o1heap/readme.md
@@ -1,2 +1,2 @@
 git@github.com:pavel-kirienko/o1heap.git 
-388a73fd9007300e5130c5fe352d9ce3288b6dde
+b21b069e4b971d3016dd232784faca6f7d9fd724
